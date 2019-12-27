e1f111873dd073ad63fcfd396c7c1f396f886919
lzc <739206442@qq.com>
2019-12-27 14:43:45 +0800

    feat：修改模板

---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+/*
+ * @Description: 说明
+ * @Author: lzc
+ * @Date: 2019-12-27 14:28:25
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = props => {
+  return (
+    <div>
+      index
+    </div>
+  );
+};
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
